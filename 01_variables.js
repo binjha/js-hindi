bd30121fc307a68ae6d0b38385b4c850899d1773
binjha <purohitvijay03112003@gmail.com>
2025-05-16 12:30:37 +0000

    container config done

---
+++ 01_variables.js
@@ -0,0 +1,18 @@
+const accountId=1245
+let accountEmil="Binjha@123gmail.com"
+var accountPassword="44550"
+accountcity="Jaipur"
+let accountstate;
+
+// accountId=2
+
+console.log(accountId);
+//please please dont use var for the variable declaration coz issue in the functional and block scope
+
+accountEmil="Rashmu@gmail.com"
+accountPassword="6266"
+accountcity="Mumbai"
+
+console.table([accountId,accountEmil,accountPassword,accountId,accountstate]);
+
+
---
+++ 02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// alert(3+3)
+
+console.log(3+3)
+console.log("Binjha")
+
+
+let name ="Rashmu"
+let age =20
+let isLoggedIn=false
