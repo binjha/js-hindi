fa8619f47f5ac89e2169e9a28e36834d859ef937
binjha <purohitvijay03112003@gmail.com>
2025-05-16 13:40:19 +0000

    conversion of datatypes

---
+++ 03_conversionoperation.js
@@ -0,0 +1,36 @@
+// let score=33
+
+// let score ="33"
+// let score="33abc"
+// let score =null
+// let score=undefined
+// let score=true
+// let score="Binjha"
+
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+
+// let isLoggedin=1
+// let isLoggedin=0
+// let isLoggedin=""
+// let isLoggedin="Binjha"
+// let isLoggedin=44
+// let booleanIsLoggedIn= Boolean(isLoggedin)
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber=44
+let StringNumber=String(someNumber)
+console.log(StringNumber);
+console.log( typeof(StringNumber));
+
+
+
