ff3fd9c96bca43b35a61957dbdf4b496029899ab
binjha <purohitvijay03112003@gmail.com>
2025-05-16 12:39:03 +0000

    datatypes

--- 02_datatypes.js
+++ 02_datatypes.js
@@ -9,3 +9,25 @@ console.log("Binjha")
 let name ="Rashmu"
 let age =20
 let isLoggedIn=false
+let state =null
+
+/*
+number = 2 to  pwer 53
+bigint
+boolean =true/fasle 
+null=standalone value
+undefined=value not assigned
+symbol we will see later used for uniqueness
+
+object
+*/
+console.log(typeof "Binjha")
+console.log(typeof age)
+console.log(typeof null)//null consider as a object
+console.log(typeof undefined)
+
+
+
+
+
+
