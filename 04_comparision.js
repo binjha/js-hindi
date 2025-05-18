22501895268f659aed91178191336d6b5d7eeae6
binjha <purohitvijay03112003@gmail.com>
2025-05-18 04:48:19 +0000

    comparison

---
+++ 04_comparision.js
@@ -0,0 +1,15 @@
+console.log("2">1);
+console.log("02">1);
+
+//js converts string to number 
+console.log("binjha">2);    
+console.log(null>1);
+console.log(null>=0);
+
+
+console.log("2"===2);
+
+let bigNum=555555555555555555555555555555555555555555555555555555555
+console.log(typeof bigNum);
+
+
