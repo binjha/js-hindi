197d67c78f1872f0697e836c6f4652c780909a35
binjha <purohitvijay03112003@gmail.com>
2025-05-18 05:16:57 +0000

    all together

--- 04_comparision.js
+++ 04_comparision.js
@@ -1,15 +1,44 @@
-console.log("2">1);
-console.log("02">1);
+// console.log("2">1);
+// console.log("02">1);
 
-//js converts string to number 
-console.log("binjha">2);    
-console.log(null>1);
-console.log(null>=0);
+// //js converts string to number 
+// console.log("binjha">2);    
+// console.log(null>1);
+// console.log(null>=0);
 
 
-console.log("2"===2);
+// console.log("2"===2);
 
-let bigNum=555555555555555555555555555555555555555555555555555555555
-console.log(typeof bigNum);
+// let bigNum=555555555555555555555555555555555555555555555555555555555
+// console.log(typeof bigNum);
+
+
+//stack and heap memory 
+
+let myname="binjha"
+let calledname=myname
+calledname="binjhuuuuu"
+
+console.log(calledname);
+console.log(myname);
+
+
+let user={
+    age:18,
+    name:"binjha"
+}
+
+let user2={
+    email:"binjha@gmail.com",
+    upi:"9024@ybl"  
+}
+
+let user3 =user2
+user3.email="cabhavana@gmail.com"
+
+console.log(user2.email);
+console.log(user3.email);
+
+
 
 
