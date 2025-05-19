b848c82610a424c9dc5cef91061f8769e7374b63
binjha <purohitvijay03112003@gmail.com>
2025-05-19 02:27:12 +0000

    strings

---
+++ 05_strings.js
@@ -0,0 +1,46 @@
+const name ="Bhavana "
+const repocount =50
+
+// console.log(name + repocount+ " binjha" ); but we dont use this 
+
+console.log(`hello my name is ${name} and repocount is ${repocount}`);
+ const gameName= new String('Bhavana is ca')
+
+ console.log(gameName);
+ console.log(gameName[0]);
+
+ console.log(gameName.__proto__);
+ console.log(gameName.length);
+ console.log(gameName.endsWith());
+ console.log(gameName.toLowerCase());
+ console.log(gameName.toUpperCase());
+
+ console.log(gameName.charAt(5));
+ console.log(gameName.indexOf('v'));
+
+
+ const newstring=gameName.slice(-8,4)
+ const anotherString=gameName.substring(0,7)
+
+
+
+ const newString1='         binjha 1'
+ console.log(newString1);
+ console.log(newString1.trim());
+ 
+ 
+
+const url ="https://binjha.com/binjhs pookie" 
+console.log(url);
+const urll ="https://binjha.com/binjhs%20pookie"
+
+console.log(urll.replace('%20','-'));
+
+console.log(url.includes('sundar'))
+
+
+
+ 
+
+ 
+
