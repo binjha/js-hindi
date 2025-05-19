990d493ec450026f868eb29d6286065159bf58d1
binjha <purohitvijay03112003@gmail.com>
2025-05-19 03:23:27 +0000

    math and num

---
+++ 06_numbsandmaths.js
@@ -0,0 +1,46 @@
+// const score=400
+// console.log(score);
+
+// const balance =new Number(200)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber=123.25
+// console.log(otherNumber.toPrecision(6));
+
+// const hundredes=100000000000;
+// console.log(hundredes.toLocaleString('en-IN'));
+
+// //maths
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+// console.log(Math.min(5,7,8,4,1));
+// console.log(Math.max(5,7,8,4,1));
+
+ console.log(Math.random());
+ console.log((Math.random()*10) +1);    
+ console.log((Math.random()*10) +1);    
+
+ const min =10
+ const max=20
+
+ console.log(Math.floor(Math.random() *(max-min+1))+min)
+
+
+
+
+
+
+
+
+
+
+
+
+
