29aefab1125edd9608fb25ab5d4d1e11210400d8
binjha <purohitvijay03112003@gmail.com>
2025-05-19 04:33:58 +0000

    date

---
+++ 07_dates.js
@@ -0,0 +1,19 @@
+let myDate =new Date()
+// console.log(myDate);
+//  console.log(myDate.toString());
+
+//  console.log(myDate.toJSON());
+//  console.log(myDate.toDateString());
+//  console.log(myDate.toTimeString());
+//  console.log(myDate.toLocaleString());
+
+// let mycreateddate=new Date(2025,10,3)
+let mycreateddate=new Date("03-11-2003")
+
+console.log(mycreateddate.toDateString());
+
+let mytimestamps=Date.now()
+console.log(mytimestamps);
+
+
+
