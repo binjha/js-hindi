206ca68df4f8ce09d243120cb25391eaffafea3c
binjha <purohitvijay03112003@gmail.com>
2025-05-20 18:09:48 +0000

    object

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,21 @@
+//singleton object created by constructor
+// Object.create
+
+
+//object literals
+const Jsuser={
+    name:"Binjha",
+    "name1":"Rashmu",
+    age:21,
+    location:"Delhi",
+    email:"binjha@gmail.com",
+    isLoggedIn:false,
+    lastloginDays:["Monday","tuesday","wednesday","Saturday"]
+}
+
+console.log(Jsuser.email);
+console.log(Jsuser["email"]);
+console.log(Jsuser.name1);
+
+
+
