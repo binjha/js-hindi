9718701330ff80ecb2fb5e0f2c7701448dc88360
binjha <purohitvijay03112003@gmail.com>
2025-05-20 03:59:10 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,51 @@
+//arrays
+
+const myarray=[0,1,2,3,4,5,"binjha",true]
+// console.log(myarray);
+
+// console.log(myarray[4]);
+
+const myarray2=new Array(1,2,3,4,5)
+
+// myarray.push(6)
+// console.log(myarray);
+// myarray.push(7)
+// console.log(myarray);
+// myarray.pop()
+
+// myarray.unshift(5)//to insert an element at the 0 insex of the array
+// myarray.shift()//remove 0 element of the array
+
+// console.log(myarray.includes(5));
+// console.log(myarray.indexOf(3));
+
+// const newarray=myarray.join()
+// console.log(newarray);//array will be same but the change is it convert it into string 
+// console.log(typeof newarray);
+// console.log(typeof myarray);
+
+// console.log(myarray);
+
+//slice and splice
+
+console.log("A",myarray);
+
+const myarr1=myarray.slice(1,3)
+console.log(myarr1);
+console.log("B",myarray);
+
+const myarr2=myarray.splice(1,3)    
+console.log("c",myarray);
+console.log(myarr2);
+
+
+
+
+
+
+
+
+
+
+
+
