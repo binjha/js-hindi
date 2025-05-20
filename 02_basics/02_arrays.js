1ba43d3410263fe3b02ee811a13c312e1235ff53
binjha <purohitvijay03112003@gmail.com>
2025-05-20 13:05:10 +0000

    array

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,38 @@
+const marval_heroes=["thor","ironaman","hulk"]
+const dc_heroes=["superman","flash","batman"]
+
+// marval_heroes.push(dc_heroes)
+
+// console.log(marval_heroes);
+// console.log(marval_heroes[3][1]);
+
+//  const allHeroes=marval_heroes.concat(dc_heroes)
+//  console.log(allHeroes);
+ 
+//  const all_new_heroes=[...marval_heroes,...dc_heroes]//this is spreading
+// console.log(all_new_heroes);
+
+// const another_array=[1,2,3,[4,5,6,[7,8,9],10,11,[12,13]]]
+// console.log(another_array);
+
+// const real_array=another_array.flat(Infinity)
+// console.log(real_array);
+
+
+// console.log(Array.isArray("Hitesh"));
+// console.log(Array.from("Hitesh"));
+
+// console.log(Array.from({name:"Binjha"}));
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3));
+
+
+
+
+
+
+
