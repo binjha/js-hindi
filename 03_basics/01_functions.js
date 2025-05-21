216cb9f8e5e1ffb09f42fe3a9227e43eca42b84d
binjha <purohitvijay03112003@gmail.com>
2025-05-21 05:44:17 +0000

    function delayed

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,25 @@
+function saymyname(){
+console.log("b");
+console.log("i");
+console.log("n");
+console.log("j");
+console.log("h");
+console.log("a");
+
+
+}
+// saymyname()
+
+function addtwonum(num1,num2) {
+    console.log(num1+num2);
+    
+}
+// addtwonum(5,7)
+addtwonum(5,"7")
+addtwonum(5,"a")
+addtwonum("5",7)
+addtwonum("5","7")
+addtwonum("a","b")
+
+
+
