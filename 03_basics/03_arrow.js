f175980668885d04fd6245b2853cae9985e2c753
binjha <purohitvijay03112003.com>
2025-06-05 17:38:44 +0530

    commit

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -15,13 +15,51 @@ const user ={
 
 // console.log(this);
 
-function chai() {
-    console.log("binjha ");
+// function chai() {
+//     console.log("binjha ");
+//     let username="rashmi "
     
-    console.log(this);
+//     console.log(this.username);
+    
+    
+// }
+// chai();
+// const chai=function () {
+//     console.log("binjha ");
+//     let username="rashmi "
+    
+//     console.log(this.username);
+    //still undefined coz of this 
+    
+// }
+// chai();
+
+const chai=  () => {
+    console.log("binjha ");
+    let username="rashmi "
+    
+    console.log(this);//if we use this will give a empty objext  and we use .username it will give undefined
     
     
 }
-chai();
+//chai();
+
+// const addtwo =(num1,num2) => {
+//      return num1+num2;
+// }
+
+//const addtwo =(num1,num2) => num1+num2;//this is also arroww function 
+const addtwo =(num1,num2) => (num1+num2)//this is also arroww function 
+
+const name =(num1,num2)=>({username :"binjha "})
+console.log(name(4,5));
+
+
+
+console.log(addtwo(5,4));
+
+
+
+
 
 
